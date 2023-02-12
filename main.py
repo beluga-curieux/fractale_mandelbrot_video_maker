df2d126df1c430fd306c42e287f554f329496ee7
belug <beluga.curieux@gmail.com>
2023-02-12 06:58:02 +0100

    couleur gradation

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import PIL.Image
 
 
-size = 5_000, 5_000
-axe_x = -4, 4
-axe_y = -4, 4
+size = 10_000, 10_000
+axe_x = -2, 2
+axe_y = -2, 2
 
 c_global = -1.755
 max_iteration = 100
@@ -22,27 +22,29 @@ def julia(c: complex | complex):
 
             z = pixel_to_complex(x, y)
 
-            if test_diverge(z, c):
-                img.putpixel((x, y), (255, 255, 255))
+            res = test_diverge(z, c)
+            co = int(255*res)
 
+            if res:
+                img.putpixel((x, y), (co, co, co))
             else:
                 img.putpixel((x, y), (0, 0, 0))
 
 
-def test_diverge(z: complex, c: complex | int) -> bool:
+def test_diverge(z: complex, c: complex | int) -> float:
     i = 0
     while z.real < 4 and i < max_iteration:
         z = (z*z)+c
         i += 1
 
-    return max_iteration == i
+    return i / max_iteration
 
 
 def pixel_to_complex(x: int, y: int) -> complex:
     return complex(((x / size[0]) * x_len) + axe_x[0], ((y/size[1]) * y_len) + axe_y[0])
 
 
-def main():
+def gen_plusieur():
     global max_iteration
 
     repertoire = f"julia_c={c_global}_evolution"
@@ -54,6 +56,13 @@ def main():
         max_iteration = loop
         julia(c_global)
         img.save(repertoire + f"/julia_c={c_global}_size={size}_pres={max_iteration}_axex={axe_x}_axe_y={axe_y}.png")
+
+
+def main():
+    julia(c_global)
+    img.save(f"julia_c={c_global}_size={size}_pres={max_iteration}_axex={axe_x}_axe_y={axe_y}.png")
+
+    img.show()
 
 
 if __name__ == '__main__':

d0478419685d94444c1bcb1bfded9079968ca51c
belug <beluga.curieux@gmail.com>
2023-02-12 17:34:04 +0100

    vidéo a partire d'un fichié est opérationelle

--- generate_julia.py
+++ generate_julia.py
@@ -4,17 +4,31 @@ from datetime import datetime
 import PIL.Image
 
 
-size = 1_000, 1_000
+size = 1_920 * 2, 1_080 * 2
+
 axe_x = -2, 2
 axe_y = -2, 2
 
 c_global = -1.755
 max_iteration = 100
+tollerense = 4
 
 x_len = axe_x[1] - axe_x[0]
 y_len = axe_y[1] - axe_y[0]
 
 img = PIL.Image.new('RGB', size)
+
+
+def center_ortonorme(x_axe=4):
+    global axe_x, axe_y, x_len, y_len
+
+    y_axe = size[1] / size[0] * x_axe
+
+    axe_x = 0 - x_axe / 2, x_axe/2
+    axe_y = 0 - y_axe / 2, y_axe/2
+
+    x_len = axe_x[1] - axe_x[0]
+    y_len = axe_y[1] - axe_y[0]
 
 
 def julia(c: complex | complex):
@@ -24,12 +38,12 @@ def julia(c: complex | complex):
             z = pixel_to_complex(x, y)
 
             res = test_diverge(z, c)
-            co = int(255*res)
+            co = int(255 - (255 * res))
 
             if res:
                 img.putpixel((x, y), (co, co, co))
             else:
-                img.putpixel((x, y), (0, 0, 0))
+                img.putpixel((x, y), (255, 255, 255))
 
 
 def test_diverge(z: complex, c: complex | int) -> float:
@@ -48,20 +62,22 @@ def pixel_to_complex(x: int, y: int) -> complex:
 def gen_plusieur():
     global max_iteration
 
-    repertoire = f"julia_c={c_global} {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}"
+    repertoire = f"julia_c={c_global} size={size} xax={axe_x} yax={axe_y} {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}"
 
     if not os.path.exists(repertoire):
         os.makedirs(repertoire)
 
-    for loop in range(1, 30):
+    for loop in range(1, 60):
+        print(loop)
         max_iteration = loop
         julia(c_global)
-        img.save(repertoire + f"/julia_c={c_global}_size={size}_pres={max_iteration}_axex={axe_x}_axe_y={axe_y}.png")
+        img.save(repertoire + f"/{loop}.png")
 
 
 def main():
+    center_ortonorme(1)
     gen_plusieur()
-    img.save(f"julia_c={c_global}_size={size}_pres={max_iteration}_axex={axe_x}_axe_y={axe_y}.png")
+    # img.save(f"julia_c={c_global}_size={size}_pres={max_iteration}_axex={axe_x}_axe_y={axe_y}.png")
 
     img.show()
 
--- img_array_using.py
+++ img_array_using.py
@@ -1,37 +1,65 @@
-import pprint
+from datetime import datetime
+import os
+from time import strftime
+
 import cv2
 
 import PIL.Image
 import numpy as np
 
 
-img = PIL.Image.open("julia_c=-1.755_size=(10000, 10000)_pres=100_axex=(-2, 2)_axe_y=(-2, 2).png")
-array_img = np.array(img)
+# img = PIL.Image.open("julia_c=-1.755_size=(10000, 10000)_pres=100_axex=(-2, 2)_axe_y=(-2, 2).png")
+# array_img = np.array(img)
 
-size_var = np.array(array_img.shape[:2])
+# size_var = np.array(array_img.shape[:2])
 
 border = 1
 pas = 25
 finit = (1_000, 1_000)
 
 video_resolution = 2000, 2000
+video_title = f"video {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}"
 video = cv2.VideoWriter('video7.avi', cv2.VideoWriter_fourcc(*'MP4v'), 60, video_resolution)
 
 
-def video_im_dict(path: dict):
-    pass
+def video_init(resolution, fps=60):
+    global video_resolution, video_title, video
+    video_resolution = resolution
+    video_title = f"video {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}.avi"
+    video = cv2.VideoWriter(video_title, cv2.VideoWriter_fourcc(*'MP4v'), fps, video_resolution)
 
 
-while tuple(size_var) != finit and size_var[0] > finit[0] and size_var[1] > finit[1]:
-    print("boucle", size_var, finit)
+def video_from_dir(dir_path: str):
+    i = 1
+    path = dir_path+"/"+str(i)+".png"
+    img = PIL.Image.open(path)
+    video_init(img.size, 5)
 
-    fram_array = array_img[border:-border, border:-border]
-    frame = PIL.Image.fromarray(fram_array).resize(video_resolution)
-    video.write(np.array(frame))
+    while os.path.exists(path):
+        print(i)
+        video.write(np.array(img))
 
-    size_var = np.array(fram_array.shape[:2])
-    border += pas
+        i += 1
+
+        img = PIL.Image.open(path)
+        path = dir_path+"/"+str(i)+".png"
+
+
+def main():
+    video_from_dir("julia_c=-1.755 size=(3840, 2160) xax=(-0.5, 0.5) yax=(-0.28125, 0.28125) 2023-02-12_17-03-31")
+
+
+
+# while tuple(size_var) != finit and size_var[0] > finit[0] and size_var[1] > finit[1]:
+#     print("boucle", size_var, finit)
+#
+#     fram_array = array_img[border:-border, border:-border]
+#     frame = PIL.Image.fromarray(fram_array).resize(video_resolution)
+#     video.write(np.array(frame))
+#
+#     size_var = np.array(fram_array.shape[:2])
+#     border += pas
 
 
 if __name__ == '__main__':
-    pass
+    main()

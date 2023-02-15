466098db6cb5c1bd2ca2236b8de12fb45f296aa7
belug <beluga.curieux@gmail.com>
2023-02-15 03:28:02 +0100

    changement de l'ancodage vidéo

--- img_array_using.py
+++ img_array_using.py
@@ -18,14 +18,15 @@ pas = 25
 finit = (1_000, 1_000)
 
 video_resolution = 2000, 2000
-video_title = f"video {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}.avi"
-video = cv2.VideoWriter('video7.avi', cv2.VideoWriter_fourcc(*'mp4v'), 60, video_resolution)
+video_title = f"video {strftime('%Y-%m-%d_%H-%M-%S', datetime.now().timetuple())}.mov"
+fourc = cv2.VideoWriter_fourcc(*'MJPG')
+video = cv2.VideoWriter(video_title, fourc, 60, video_resolution)
 
 
 def video_init(resolution, fps=60):
     global video_resolution, video
     video_resolution = resolution
-    video = cv2.VideoWriter(video_title, cv2.VideoWriter_fourcc(*'MP4v'), fps, video_resolution)
+    video = cv2.VideoWriter(video_title, fourc, fps, video_resolution)
 
 
 def video_from_dir(dir_path: str):
@@ -33,10 +34,10 @@ def video_from_dir(dir_path: str):
     path = dir_path+"/"+str(i)+".png"
     img = PIL.Image.open(path)
     video_init(img.size, 30)
-
     while os.path.exists(path):
         print(i)
         video.write(np.array(img))
+        # cv2.imshow('frame', np.array(img))
 
         i += 1
 
@@ -46,7 +47,7 @@ def video_from_dir(dir_path: str):
 
 def main():
 
-    video_from_dir("th pres=40 size=(1920, 1080) xax=(-2.0, 2.0) yax=(-1.125, 1.125) 2023-02-14   13-52-24")
+    video_from_dir("th pres=40 size=(1920, 1080) xax=(-2.0, 2.0) yax=(-1.125, 1.125) 2023-02-14   14-09-32")
 
 
 if __name__ == '__main__':
